8d95989bc050a0d38bda8decb854380aeece2d33
LMathers00 <lukemathers@hotmail.co.uk>
2022-07-06 15:05:25 +0100

    wrote basic skeleton for code

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-import pokemonArray from "./data/pokemon";
+import pokemonArray from "./data/pokemon.js";
 
-const container = document.querySelector(".card-container")
+const cardContainer = document.querySelector(".card-container");
 
+
+
+//Making a function to produce each card and then replicate might be easier than going directly in container html
+const makeIndividualCards = () => {
+  let IndividualCardsHtml = "";
+
+  for (const pokemon of pokemonArray) {
+    const pokemonHtml = `<div class="card">
+	  <img
+		src="${pokemon.sprite}" alt="${pokemon.name}"/>
+	  <div><h2>${pokemon.name}</h2><p>${pokemon.name} (#${pokemon.id})is a TBD type pokemon.</p></div>
+	</div>`;
+
+    IndividualCardsHtml += pokemonHtml;
+  }
+  return IndividualCardsHtml;
+};
 
