879b2ddfb1f559c0e59829aaedcc17ede0103076
LMathers00 <lukemathers@hotmail.co.uk>
2022-07-06 15:19:48 +0100

    added class tags displays with stylings + tidied code

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ import pokemonArray from "./data/pokemon.js";
 
 const cardContainer = document.querySelector(".card-container");
 
+
 //Making a function to produce each card and then replicate might be easier than going directly in container
 const makeIndividualCards = () => {
   let IndividualCardsHtml = "";
 
   for (const pokemon of pokemonArray) {
-    const pokemonHtml = `<div>
+    const pokemonHtml = `<div class="card">
 	  <img
+		class="card__image"
 		src="${pokemon.sprite}"
 		alt="${pokemon.name}"
 	  />
-	  <div><h2>${pokemon.name}</h2><p>${pokemon.name} (#${pokemon.id})is a TBD type pokemon.</p></div>
+	  <div class="card__content">
+		<h2 class="card__heading">${pokemon.name}</h2>
+		<p class="card__text">${pokemon.name} (#${pokemon.id})
+         is a TBD type pokemon.</p>
+	  </div>
 	</div>`;
 
     IndividualCardsHtml += pokemonHtml;
   }
   return IndividualCardsHtml;
 };
+
 //This should make it actually appear
 cardContainer.innerHTML = makeIndividualCards();
 
